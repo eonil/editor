82b9fad676d9d1f7d280794dff93f13c0ffbdf9e
Hoon H <drawtree@me.com>
2014-11-14 04:19:34 +0900

    Adjusted project structure. Added FSEvents wrapper.

---
+++ Project/Sources/SpecificUIComponents/FileNavigating/FileTreeViewController4.swift
@@ -0,0 +1,287 @@
+//
+//  FileTreeViewController4.swift
+//  RustCodeEditor
+//
+//  Created by Hoon H. on 11/13/14.
+//  Copyright (c) 2014 Eonil. All rights reserved.
+//
+
+import Foundation
+import AppKit
+import EonilDispatch
+
+class FileTreeViewController4 : NSViewController, NSOutlineViewDataSource, NSOutlineViewDelegate {
+	
+	let	userIsWantingToEditFileAtPath	=	Notifier<String>()
+	
+	private	var	_fileTreeRepository		=	nil as FileTreeRepository4?
+	private	var	_fileSystemMonitor		=	nil as FileSystemMonitor?
+	
+	private var	_channelsHolding:Any?
+	
+	var URLRepresentation:NSURL? {
+		get {
+			return	self.representedObject as NSURL?
+		}
+		set(v) {
+			self.representedObject	=	v
+		}
+	}
+	private func onError(e:NSError) {
+		NSAlert(error: e).runModal()
+	}
+	private func onAddingNodes(ns:[FileNode4]) {
+		let	ns2	=	ns.map({$0.supernode}).filter({$0 != nil}).map({$0!})
+		let	ns3	=	deduplicate(ns2)
+		println(ns3)
+		
+		self.outlineView.beginUpdates()
+		for n in ns3 {
+			self.outlineView.reloadItem(n, reloadChildren: true)
+		}
+		self.outlineView.endUpdates()
+	}
+	private func onRemovingNodes(ns:[FileNode4]) {
+//		let	ns2	=	ns.map({$0.supernode}).filter({$0 != nil}).map({$0!})
+//		let	ns3	=	deduplicate(ns2)
+//		
+//		self.outlineView.beginUpdates()
+//		for n in ns3 {
+//			self.outlineView.reloadItem(n, reloadChildren: true)
+//		}
+//		self.outlineView.endUpdates()
+		
+		self.outlineView.beginUpdates()
+		for n in ns {
+			let	idx1	=	self.outlineView.rowForItem(n)
+			self.outlineView.removeItemsAtIndexes(NSIndexSet(index: idx1), inParent: n.supernode!, withAnimation: NSTableViewAnimationOptions.EffectFade)
+		}
+		self.outlineView.endUpdates()
+	}
+	
+	
+	
+	
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	override var representedObject:AnyObject? {
+		willSet(v) {
+			precondition(v is NSURL)
+			_channelsHolding	=	nil
+			_fileSystemMonitor	=	nil
+			_fileTreeRepository	=	nil
+		}
+		didSet {
+			if let v3 = URLRepresentation {
+				_fileTreeRepository	=	FileTreeRepository4(rootlink: v3)
+				_fileTreeRepository!.reload()
+				
+				_fileSystemMonitor	=	FileSystemMonitor(v3)
+			}
+			self.outlineView.reloadData()
+			
+			_channelsHolding	=	connectMonitorToTree(_fileSystemMonitor!, _fileTreeRepository!, outlineView)
+		}
+	}
+	
+	var outlineView:NSOutlineView {
+		get {
+			return	self.view as NSOutlineView
+		}
+		set(v) {
+			self.view	=	v
+		}
+	}
+	override func loadView() {
+		super.view	=	NSOutlineView()
+	}
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		let	col1	=	NSTableColumn(identifier: NAME, title: "Name", width: 100)
+
+		outlineView.focusRingType	=	NSFocusRingType.None
+		outlineView.headerView		=	nil
+		outlineView.addTableColumn <<< col1
+		outlineView.outlineTableColumn		=	col1
+		outlineView.rowSizeStyle			=	NSTableViewRowSizeStyle.Small
+		outlineView.selectionHighlightStyle	=	NSTableViewSelectionHighlightStyle.SourceList
+		outlineView.sizeLastColumnToFit()
+		
+		outlineView.setDataSource(self)
+		outlineView.setDelegate(self)
+	}
+	
+	
+	
+	
+//	func outlineView(outlineView: NSOutlineView, heightOfRowByItem item: AnyObject) -> CGFloat {
+//		return	16
+//	}
+	
+	func outlineView(outlineView: NSOutlineView, numberOfChildrenOfItem item: AnyObject?) -> Int {
+		let	n1	=	item as FileNode4?
+		if let n2 = n1 {
+			if n2.subnodes.count == 0 {
+				n2.reloadSubnodes()
+				for u1 in n2.subnodes.links {
+					_fileSystemMonitor!.addWatchForURL(u1)
+				}
+			}
+			return	n2.subnodes.count
+		} else {
+			return	_fileTreeRepository?.root == nil ? 0 : 1
+		}
+	}
+	func outlineView(outlineView: NSOutlineView, child index: Int, ofItem item: AnyObject?) -> AnyObject {
+		let	n1	=	item as FileNode4?
+		if let n2 = n1 {
+			return	n2.subnodes[index]
+		} else {
+			return	_fileTreeRepository!.root!
+		}
+	}
+//	func outlineView(outlineView: NSOutlineView, isGroupItem item: AnyObject) -> Bool {
+//		let	n1	=	item as FileNode4
+//		let	ns2	=	n1.subnodes
+//		return	ns2 != nil
+//	}
+	func outlineView(outlineView: NSOutlineView, isItemExpandable item: AnyObject) -> Bool {
+		let	n1	=	item as FileNode4
+		return	n1.directory
+	}
+//	func outlineView(outlineView: NSOutlineView, objectValueForTableColumn tableColumn: NSTableColumn?, byItem item: AnyObject?) -> AnyObject? {
+//		let	n1	=	item as FileNode4
+//		return	n1.relativePath
+//	}
+	func outlineView(outlineView: NSOutlineView, viewForTableColumn tableColumn: NSTableColumn?, item: AnyObject) -> NSView? {
+		let	tv1	=	NSTextField()
+		let	iv1	=	NSImageView()
+		let	cv1	=	NSTableCellView()
+		cv1.textField	=	tv1
+		cv1.imageView	=	iv1
+		cv1.addSubview(tv1)
+		cv1.addSubview(iv1)
+
+		///	Do no query on file-system here.
+		///	The node is a local cache, and may not exist on 
+		///	underlying file-system. 
+		///	This is especially true for kqueue/GCD notification.
+		///	GCD VNODE notifications are fired asynchronously, and
+		///	the actual file can be already gone at the time of the
+		///	notification arrives. In other words, file operation
+		///	does not wait for notification returns.
+		///	So do not query on file-system at this point, and just
+		///	use cached data on the node.
+		
+		let	n1	=	item as FileNode4
+		iv1.image						=	n1.icon
+		cv1.textField!.stringValue		=	n1.displayName
+		cv1.textField!.bordered			=	false
+		cv1.textField!.backgroundColor	=	NSColor.clearColor()
+		cv1.textField!.editable			=	false
+		(cv1.textField!.cell() as NSCell).lineBreakMode	=	NSLineBreakMode.ByTruncatingHead
+		return	cv1
+	}
+	
+	
+	
+	func outlineViewSelectionDidChange(notification: NSNotification) {
+//		let	idx1	=	self.outlineView.selectedRow
+//		let	n1		=	self.outlineView.itemAtRow(idx1) as FileNode4
+//		userIsWantingToEditFileAtPath.signal(n1.absolutePath)
+	}
+	func outlineViewItemDidCollapse(notification: NSNotification) {
+		assert(notification.name == NSOutlineViewItemDidCollapseNotification)
+		let	n1	=	notification.userInfo!["NSObject"]! as FileNode4
+		for u1 in n1.subnodes.links {
+			_fileSystemMonitor!.removeWatchForURL(u1)
+		}
+		n1.resetSubnodes()
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+private let	NAME	=	"NAME"
+
+
+
+
+
+
+///	Keep the returned object to keep the connection.
+private func connectMonitorToTree(m:FileSystemMonitor, t:FileTreeRepository4, v:NSOutlineView) -> Any {
+	unowned let	v2	=	v
+	func onVNodeEvent(u:NSURL) {
+		assert(NSThread.currentThread() == NSThread.mainThread())
+		//	Updated. Includes changes in direct subnodes.
+		
+		///	A vnnode event can be arrived later, and
+		///	the node informations may be already updated 
+		///	to reflect deleted files. So a file node fot
+		///	the event may does not exists at this point.
+		if let n1 = t[u] {
+			let	r1	=	v.rowForItem(n1)
+			for u1 in n1.subnodes.links {
+				m.removeWatchForURL(u1)
+			}
+			n1.reloadSubnodes()
+			for u1 in n1.subnodes.links {
+				m.addWatchForURL(u1)
+			}
+			
+			v2.beginUpdates()
+			v2.reloadItem(n1, reloadChildren: true)
+			v2.endUpdates()
+		}
+		
+		
+	}
+	
+	let	ch1	=	channel(m.notifyEventOnWatchForURL, onVNodeEvent)
+	return	(ch1)
+}
+
+
+
+
+
+
+
+
+
+
+
---
+++ Project/Subprojects/EonilFileSystemEvents/TestdriveApp/AppDelegate.swift
@@ -0,0 +1,108 @@
+//
+//  AppDelegate.swift
+//  TestdriveApp
+//
+//  Created by Hoon H. on 11/12/14.
+//  Copyright (c) 2014 Eonil. All rights reserved.
+//
+
+import Cocoa
+import EonilFileSystemEvents
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate, NSTableViewDataSource, NSTableViewDelegate {
+
+	@IBOutlet weak var	tableView:NSTableView!
+	@IBOutlet weak var	window:NSWindow!
+
+	struct Item {
+		let	flags:String
+		let	path:String
+	}
+	
+	var	items	=	[] as [Item]
+	var	monitor	=	nil as EonilFileSystemEventStream?
+	var	queue	=	dispatch_get_global_queue(QOS_CLASS_BACKGROUND, 0)
+
+	func applicationDidFinishLaunching(aNotification: NSNotification) {
+		
+		
+		
+		/////////////////////////////////////////////////////////////////
+		//	Here's the core of example.
+		/////////////////////////////////////////////////////////////////
+		let	onEvents	=	{ (events:[AnyObject]!) -> () in
+			let	a1	=	events! as [EonilFileSystemEvent]
+			dispatch_async(dispatch_get_main_queue(), { () -> Void in
+				for e1 in a1 {
+					let	v1	=	e1.flag.rawValue
+					var	a1	=	[] as [String]
+					for i:UInt32 in 0..<16 {
+						let	v2	=	0b01 << i
+						let	ok	=	(v2 & v1) > 0
+						if ok {
+							let	s	=	NSStringFromFSEventStreamEventFlags(v2)!
+							a1.append(s)
+						}
+					}
+					
+					let	s1	=	join(", ", a1)
+					self.items.append(Item(flags: s1, path: e1.path!))
+					self.tableView.insertRowsAtIndexes(NSIndexSet(index: self.items.count-1), withAnimation: NSTableViewAnimationOptions.EffectNone)
+				}
+				self.tableView.scrollToEndOfDocument(self)
+			})
+		}
+		monitor	=	EonilFileSystemEventStream(callback: onEvents, pathsToWatch: ["/"], watchRoot: false, queue: queue)
+		/////////////////////////////////////////////////////////////////
+		//	Here's the core of example.
+		/////////////////////////////////////////////////////////////////
+		
+		
+		
+	}
+
+	func applicationWillTerminate(aNotification: NSNotification) {
+		// Insert code here to tear down your application
+	}
+
+	
+	func numberOfRowsInTableView(tableView: NSTableView) -> Int {
+		return	items.count
+	}
+//	func tableView(tableView: NSTableView, objectValueForTableColumn tableColumn: NSTableColumn?, row: Int) -> AnyObject? {
+//		let	m1	=	items[row]
+//		switch tableColumn!.identifier {
+//			case "TYPE":	return	m1.type
+//			case "PATH":	return	m1.path
+//			default:		fatalError()
+//		}
+//	}
+	func tableView(tableView: NSTableView, viewForTableColumn tableColumn: NSTableColumn?, row: Int) -> NSView? {
+		let	tv1	=	NSTextField()
+		let	iv1	=	NSImageView()
+		let	cv1	=	NSTableCellView()
+		cv1.textField	=	tv1
+		cv1.imageView	=	iv1
+		cv1.addSubview(tv1)
+		cv1.addSubview(iv1)
+		
+		cv1.textField!.bordered			=	false
+		cv1.textField!.backgroundColor	=	NSColor.clearColor()
+		cv1.textField!.editable			=	false
+		(cv1.textField!.cell() as NSCell).lineBreakMode	=	NSLineBreakMode.ByTruncatingHead
+		
+		let	n1	=	items[row]
+		switch tableColumn!.identifier {
+		case "PATH":
+			iv1.image						=	NSWorkspace.sharedWorkspace().iconForFile(n1.path)
+			cv1.textField!.stringValue		=	n1.path
+		case "TYPE":
+			cv1.textField!.stringValue		=	n1.flags
+		default:
+			break
+		}
+		return	cv1
+	}
+}
+
