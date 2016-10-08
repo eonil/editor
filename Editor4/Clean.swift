da62cc2b3ace67aa892e5a1f0a6867dddc0c980b
Hoon H <drawtree@gmail.com>
2016-10-08 17:24:37 +0900

    Erase all to rewrite from scratch.

--- Editor4/Clean.swift
+++
@@ -1,7 +0,0 @@
-//
-//  Clean.swift
-//  Editor4
-//
-//  Created by Hoon H. on 2016/05/18.
-//  Copyright © 2016 Eonil. All rights reserved.
-//
--- Editor4/UserInteractionScanning.swift
+++
@@ -1,7 +0,0 @@
-//
-//  UserInteractionScanning.swift
-//  Editor4
-//
-//  Created by Hoon H. on 2016/06/26.
-//  Copyright © 2016 Eonil. All rights reserved.
-//
